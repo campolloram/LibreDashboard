2390f899454f76048d4aa344c9399f029f2d92bd
Guillermo Campollo <campollo.ram@gmail.com>
2021-05-25 08:57:25 -0500

    Added more logic to the html

--- libredashboard/utils.py
+++ libredashboard/utils.py
@@ -2,15 +2,16 @@ import plotly
 import plotly.graph_objs as go
 import json
 
+
 def get_plot(df, x_axis):
-    print(df.head())
     data = []
     for column in df.columns:
         if column == x_axis:
             continue
-        plot = go.Bar(
-                x=df[x_axis], # assign x as the dataframe column 'x'
-                y=df[column]
-            )
+        plot = go.Scatter(
+            x=df[x_axis],  # assign x as the dataframe column 'x'
+            y=df[column],
+            name=column
+        )
         data.append(plot)
     return json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
